566a162e122fa554be0026937a18a57cdc88548c
zsy <2789459251@qq.com>
2024-04-05 21:48:41 +0800

    rpc之超时处理，http协议

--- main/main.go
+++ main/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"Geerpc"
+	"context"
+	"fmt"
 	"log"
 	"net"
+	"net/http"
+	"sync"
+	"time"
 )
 
 //	func startServer(addr chan string) {
@@ -29,13 +34,46 @@ func startServer(addr chan string) {
 	if err := Geerpc.Register(&foo); err != nil {
 		log.Fatalf("register error :", err)
 	}
-	l, err := net.Listen("tcp", ":0")
+	l, err := net.Listen("tcp", ":9999")
 	if err != nil {
 		log.Fatal("network error :", err)
 	}
 	log.Println("start rpc server on", l.Addr())
 	addr <- l.Addr().String()
-	Geerpc.Accept(l)
+	Geerpc.HandleHTTP()
+	_ = http.Serve(l, nil)
+}
+
+func call(addrCh chan string) {
+	str := <-addrCh
+	fmt.Println("call start:", str)
+	client, _ := Geerpc.DialHTTP("tcp", str)
+	fmt.Println("client:", client)
+	defer func() { _ = client.Close() }()
+
+	time.Sleep(time.Second)
+	// send request & receive response
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
+			if err := client.Call(context.Background(), "Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
+		}(i)
+	}
+	wg.Wait()
+}
+
+func main() {
+	log.SetFlags(0)
+	ch := make(chan string)
+	go call(ch)
+	startServer(ch)
 }
 
 //
